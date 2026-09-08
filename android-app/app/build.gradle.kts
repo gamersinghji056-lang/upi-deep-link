67fa559afe5e82042a7559fc85d3d7d79e17fb6b
gamersinghji056-lang <gamersinghji056@gmail.com>
2026-09-09 02:30:24 +0530

    Bump APK version for OTP repair

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.wtron.wpayagent"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.10.0"
+        versionCode = 12
+        versionName = "0.10.1"
         buildConfigField("String", "API_BASE_URL", "\"https://pay.wtron.org\"")
     }
 
