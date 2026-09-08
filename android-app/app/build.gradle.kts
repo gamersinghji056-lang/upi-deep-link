40a8dc8688b6a61767536b374e3b4ad99558073a
gamersinghji056-lang <gamersinghji056@gmail.com>
2026-09-08 14:46:06 +0530

    Bump WPAY Agent to v0.3.0

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.wtron.wpayagent"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
         buildConfigField("String", "API_BASE_URL", "\"https://pay.wtron.org\"")
     }
 
